b8a1e89b9101067a60c2a32684411530878c15c7
Willian_Gasparini <wiiillian21@gmail.com>
2024-01-03 20:26:06 -0300

    Projeto finalizado

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+// Components
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+import { FiSend } from 'react-icons/fi'
+import UserForm from './components/UserForm'
+import ReviewForm from './components/ReviewForm'
+import Thanks from './components/Thanks'
+import Steps from './components/Steps'
+
+
+// Hooks
+import { useForm } from './hooks/useForm'
+import { useState } from 'react'
+
+// css
+import './App.css'
+
+const formTemplate = {
+  name: "",
+  email: "",
+  review: "",
+  comment: "",
+};
+
+function App() {
+  const [data, setData] = useState(formTemplate);
+
+  const updateFieldHandler = (key, value) => {
+    setData((prev) => {
+      return {...prev, [key]: value};
+    });
+  }
+
+  const formComponenets = [
+    <UserForm  data={data} updateFieldHandler={updateFieldHandler} />,
+    <ReviewForm data={data} updateFieldHandler={updateFieldHandler}/>,
+    <Thanks data={data} />,
+  ]
+
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirtsStep } = useForm(formComponenets);
+
+  return (
+    <div className='app'>
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, ultilize o formúlario abaixo para avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <Steps currentStep={currentStep}/>
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
+          <div className="inputs-container"> {currentComponent} </div>
+          <div className="actions">
+
+            {!isFirtsStep && (
+              <button type='button' onClick={() => changeStep(currentStep - 1)}>
+                <GrFormPrevious />
+                <span>Voltar</span>
+              </button>
+            )}
+
+            {!isLastStep ? (
+              <button type='submit'>
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+
+            ) : (
+              <button type='button' onClick={() => alert('Enviado com Sucesso, Obrigado pela avaliação')}>
+                <span>Enviar</span>
+                <FiSend />
+              </button>
+            )}
+
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
